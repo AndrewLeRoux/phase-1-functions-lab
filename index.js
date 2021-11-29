550dc97d4cd6ae8f6b07a224e12bdf94b0029b26
Andrew LeRoux <andrewleroux2639@gmail.com>
2021-11-29 11:53:14 -0800

    commit tree c1e854a529b47781e6918591c9bd595c9bf21dfb

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+  }
+
+
+  function distanceFromHqInFeet(someValue) {
+    return 264 * distanceFromHqInBlocks(someValue);
+  }
+
+
+  function distanceTravelledInFeet(start, destination) {
+    return 264 * Math.abs(start - destination);
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const fareDistance = distanceTravelledInFeet(start, destination);
+    if (fareDistance < 400) {
+        return 0;
+    }
+    else if (fareDistance >= 400 && fareDistance <= 2000) {
+        return (fareDistance - 400) * 0.02;
+    }
+    else if (fareDistance > 2000 && fareDistance < 2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+
+}
